49050778ea2f0de5f679d3d32551d180bfd045ce
Jose Moreira <ei12002@fe.up.pt>
2015-09-07 18:05:36 +0100

    Lib working now

--- libraries/tess-two/bin/AndroidManifest.xml.d
+++ libraries/tess-two/bin/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/mppl/Downloads/tess-two-master/tess-two/bin/AndroidManifest.xml : \
-/Users/mppl/Downloads/tess-two-master/tess-two/AndroidManifest.xml \
+/Users/mppl/Dev/Android/godutch/libraries/tess-two/bin/AndroidManifest.xml : \
+/Users/mppl/Dev/Android/godutch/libraries/tess-two/AndroidManifest.xml \
