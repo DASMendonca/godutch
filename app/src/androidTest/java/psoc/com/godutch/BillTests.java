89f8fcb60a6ababb6bcba8b34fb1bccedf25d056
dasmendonca <ei12167@fe.up.pt>
2015-09-10 17:03:01 +0100

    Tracking test files

---
+++ app/src/androidTest/java/psoc/com/godutch/BillTests.java
@@ -0,0 +1,12 @@
+package psoc.com.godutch;
+
+import android.test.InstrumentationTestCase;
+import psoc.com.godutch.*;
+
+/**
+ * Created by asmen on 10/09/2015.
+ */
+public class BillTests extends InstrumentationTestCase {
+
+
+}
