e9ce737634f0402f36247cb3e4cb60c323c236ce
dasmendonca <ei12167@fe.up.pt>
2015-09-10 15:08:54 +0100

    still working on Bill Activity setting adapter class for the listview, People and Bill rows

--- app/src/main/java/psoc/com/godutch/BillAdapter.java
+++ app/src/main/java/psoc/com/godutch/BillAdapter.java
@@ -1,22 +1,14 @@
 package psoc.com.godutch;
 
-import android.app.ActionBar;
-import android.app.Activity;
 import android.content.Context;
-import android.os.Build;
-import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-import java.io.LineNumberReader;
-import java.text.AttributedCharacterIterator;
 
 /**
  * Created by asmen on 09/09/2015.
@@ -58,13 +50,18 @@ public class BillAdapter extends ArrayAdapter<BillRow> {
         productDescription.setText(billRow.getProductDescription());
         productPrice.setText(String.valueOf(billRow.getPrice()));
 
-        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(people.getLayoutParams().WRAP_CONTENT, people.getLayoutParams().WRAP_CONTENT);
-        for(Person person : billRow.getPersons()){
-            TextView textView = new TextView(people.getContext()); //not sure if it is the right context
-            textView.setBackgroundResource(R.drawable.textview_circle_design);
-            textView.setText(person.getShortName());
-            textView.setLayoutParams(layoutParams);
+       // FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(
+         //       people.getLayoutParams().WRAP_CONTENT, people.getLayoutParams().WRAP_CONTENT);
 
+        //using this layoutParams instead just for testing purposes
+         FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(30, 30);
+
+        //add the people that are set to share this bill
+        for(Person person : billRow.getPersons()){
+            Button button = new Button(people.getContext()); //not sure if it is the right context
+            button.setBackgroundResource(R.drawable.button_circle_design);
+            button.setText(person.getShortName());
+            button.setLayoutParams(layoutParams);
         }
 
         return row;
